f251fff696c801ac592f386d759d2cbbf1cd815a
cdnoo <89635373+cdnoo@users.noreply.github.com>
2021-08-30 22:51:16 +0800

    Delete navtj.js

--- navtj.js
+++
@@ -1,8 +0,0 @@
-document.write("	<dd><a href=\"https:\/\/\/\">UU视频<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
