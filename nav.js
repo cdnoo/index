325cefeccb670843c2eb533cd9c2166cb4826f8f
cdnoo <89635373+cdnoo@users.noreply.github.com>
2021-08-30 23:09:46 +0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,8 +1,8 @@
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">UU视频<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
-document.write("	<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
+document.write("<dd><a href=\"https:\/\/www.uuloc.com\/\">UU视频<\/a><\/dd>");
+document.write("<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
+document.write("<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
+document.write("<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
+document.write("<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
+document.write("<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
+document.write("<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
+document.write("<dd><a href=\"https:\/\/www.uuloc.com\/\">精品推荐<\/a><\/dd>");
